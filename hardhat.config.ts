412f5232101a2fc086bb22727ac7105d9638e930
Antonio Sanso <antonio.sanso@gmail.com>
2025-02-13 11:28:46 +0100

    restore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,12 @@
 import '@nomiclabs/hardhat-waffle'
 import '@typechain/hardhat'
-import { HardhatUserConfig, subtask,task } from 'hardhat/config'
+import { HardhatUserConfig, task } from 'hardhat/config'
 import 'hardhat-deploy'
 import '@nomiclabs/hardhat-etherscan'
 
 import 'solidity-coverage'
 
 import * as fs from 'fs'
-
-const {
-  TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD,
-} = require("hardhat/builtin-tasks/task-names");
-const path = require("path");
 
 const SALT = '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3'
 process.env.SALT = process.env.SALT ?? SALT
@@ -44,45 +39,6 @@ const optimizedCompilerSettings = {
   }
 }
 
-const optimizedCompilerSettingsFalcon = {
-  version: '0.8.28',
-  settings: {
-    evmVersion: 'cancun',
-    optimizer: { enabled: false},
-    viaIR: false
-  }
-}
-
-
-subtask(
-  TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD,
-  async (
-    args: {
-      solcVersion: string;
-    },
-    hre,
-    runSuper
-  ) => {
-    console.log("subtask TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD");
-    if (process.env.SOLC !== undefined) {
-      console.log(
-          "Using Solidity compiler set in SOLC:",
-          process.env.SOLC
-      );
-      return {
-        compilerPath: process.env.SOLC,
-        isSolcJs: false, // false for native compiler
-        version: args.solcVersion,
-      };
-    }
-    console.log("snooooo");
-    // since we only want to override the compiler for version 0.8.24,
-    // the runSuper function allows us to call the default subtask.
-    return runSuper();
-  }
-);
-
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -97,12 +53,9 @@ const config: HardhatUserConfig = {
       }
     }],
     overrides: {
-      'contracts/core/EntryPoint.sol': optimizedCompilerSettingsFalcon,
+      'contracts/core/EntryPoint.sol': optimizedCompilerSettings,
       'contracts/core/EntryPointSimulations.sol': optimizedCompilerSettings,
-      'contracts/samples/SimpleAccount.sol': optimizedCompilerSettingsFalcon,
-      'contracts/samples/SimpleAccountFactory.sol': optimizedCompilerSettingsFalcon,
-      'contracts/samples/NTT.sol': optimizedCompilerSettingsFalcon, 
-      'contracts/samples/Falcon.sol': optimizedCompilerSettingsFalcon, 
+      'contracts/samples/SimpleAccount.sol': optimizedCompilerSettings
     }
   },
   networks: {
